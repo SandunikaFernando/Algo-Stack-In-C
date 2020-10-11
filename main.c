16723c3d7cc04673560ccfee8a7e8975ed36542f
AvishkaSooriyapperuma <[avishkadilshan400@gmail.com]>
2020-10-11 22:29:48 +0530

    From Avishka

--- main.c
+++ main.c
@@ -1,19 +1,36 @@
 #include<stdio.h>
 struct Stack{
-    
+
     int top,arr[3];
-    
+
 }i;
 
-// push cunction here
+void push(int data){​​​​​​​
 
-// pop function here
+    i.top++;
+
+    i.arr[i.top]=data;
+
+}​​​​​​​
+
+int pop(){​​​​​​​
+
+    int data;
+
+    data=i.arr[i.top];
+
+    i.top--;
+
+    return data;
+
+}​​​​​​​
+
 
 // is full function here
 
 // is empty function here
 
 int main(){
-    
+
    // call to the functions and create a text menu
 }
