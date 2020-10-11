ea57505a272cab4a8bcde729ed189d5f31dcf324
Thilina-Pathirage <thilinapathirage@gmail.com>
2020-10-11 22:16:52 +0530

    Update

---
+++ main.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+struct Stack{
+    
+    int top,arr[3];
+    
+}i;
+
+// push cunction here
+
+// pop function here
+
+// is full function here
+
+// is empty function here
+
+int main(){
+    
+   // call to the functions and create a text menu
+}
